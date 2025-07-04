216e5a388772c3a08523031c80aa649a8ec033a9
marccvictoria <marctristanvictoria@gmail.com>
2025-07-04 11:32:51 +0800

    swap.c fix

--- C5 - Pointers and Array/2. Pointers and Function Arguments/swap.c
+++ C5 - Pointers and Array/2. Pointers and Function Arguments/swap.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-int main()
-{
-    swap(1, 2);
-}
-
 void swap(int *x, int *y)
 {
+    int temp = *x;
     *x = *y;
-    *y = *x;
-    printf("%d, %d\n", x, y);
+    *y = temp;
+    printf("%d, %d\n", *x, *y);
+}
+
+int main()
+{
+    int a = 1, b = 2;
+    printf("%d, %d\n", a, b);
+    swap(&a, &b);
+    printf("%d, %d\n", a, b); // the value is changed even in main() since pointers change addresses permanently
 }
