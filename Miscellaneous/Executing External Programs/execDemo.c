9c1669b7327613889a9435d3f4eaae4b16964220
marccvictoria <marctristanvictoria@gmail.com>
2025-07-05 09:54:14 +0800

    updated execDemo.c

--- Miscellaneous/Executing External Programs/execDemo.c
+++ Miscellaneous/Executing External Programs/execDemo.c
@@ -5,19 +5,17 @@
 #include <unistd.h>
 int main()
 {
-    // A null terminated array of character
-    // pointers
-    char *args[] = {"./exec", NULL};
+    // A null terminated array of character pointers
+    char *args[] = {"/usr/bin/ls", "-l", NULL}; // $PATH
     execvp(args[0], args);
     /*
     Syntax:
     int execvp (const char *file, char *const argv[]);
-    */
 
-    /*All statements are ignored after execvp() call as this whole
+    - All statements are ignored after execvp() call as this whole
     process(execDemo.c) is replaced by another process (EXEC.c)
     */
-    printf("Ending-----");
+    printf("Failed to Execute\n");
 
     return 0;
 }
